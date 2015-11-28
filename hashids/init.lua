9923cbbab716a1264b64cc1d4591b32a37b8486f
Dwayne Slater <ds84182@gmail.com>
2015-11-28 13:33:09 -0500

    12% speed increase

--- hashids/init.lua
+++ hashids/init.lua
@@ -4,6 +4,10 @@ local pow    = math.pow
 local substr = string.sub
 local upcase = string.upper
 local format = string.format
+local gsub = string.gsub
+local find = string.find
+local byte = string.byte
+local gmatch = string.gmatch
 local strcat = table.concat
 local push   = table.insert
 local str_switch_pos
@@ -14,9 +18,9 @@ if ok then
 else
 	str_switch_pos = function(str, pos1, pos2)
 		pos1 = pos1 + 1; pos2 = pos2 + 1;
-		local a,b = str:sub(pos1,pos1), str:sub(pos2, pos2);
-		if pos1 > pos2 then return str:gsub(a, b, 1):gsub(b, a, 1); end
-		return str:gsub(b, a, 1):gsub(a, b, 1);
+		local a,b = substr(str,pos1,pos1), substr(str, pos2, pos2);
+		if pos1 > pos2 then return gsub(gsub(str, a, b, 1), b, a, 1); end
+		return gsub(gsub(str, b, a, 1), a, b, 1);
 	end
 end
 
@@ -26,39 +30,41 @@ hash_mt.__index = hash_mt;
 
 local function gcap(str, pos)
 	pos = pos + 1;
-	return str:sub(pos, pos);
+	return substr(str, pos, pos);
 end
 
 -- TODO using string concatenation with .. might not be the fastest in a loop
 local function hash(number, alphabet)
-	local hash, alen = "", alphabet:len();
+	local alen = #alphabet;
+	local buffer = {}
+	
 	repeat
-		hash = gcap(alphabet, (number % alen)) .. hash;
+		push(buffer, 1, gcap(alphabet, (number % alen)))
 		number = floor(number / alen);
 	until number == 0
 
-	return hash;
+	return strcat(buffer);
 end
 
 local function unhash(input, alphabet)
-	local number, ilen, alen = 0, input:len(), alphabet:len();
+	local number, ilen, alen = 0, #input, #alphabet;
 
 	for i=0, ilen do
-		local cpos = (alphabet:find(gcap(input, i), 1, true) - 1);
-		number = number + cpos * pow(alen, (ilen - i - 1))
+		local cpos = (find(alphabet, gcap(input, i), 1, true) - 1);
+		number = number + cpos * (alen^(ilen - i - 1))
 	end
 
 	return number;
 end
 
 local function consistent_shuffle(alphabet, salt)
-	local slen = salt:len();
+	local slen = #salt;
 	if slen == 0 then return alphabet end
 
 	local v, p = 0, 0;
-	for i = (alphabet:len() - 1), 1, -1 do
+	for i = (#alphabet - 1), 1, -1 do
 		v = (v % slen);
-		local ord = gcap(salt, v):byte();
+		local ord = byte(salt, v+1)
 		p = p + ord;
 		local j = (ord + v + p) % i;
 
@@ -70,54 +76,53 @@ local function consistent_shuffle(alphabet, salt)
 end
 
 function hash_mt:encode(...)
-	local numbers = {select(1,...)};
+	local numbers = {...};
 	local numbers_size, hash_int = #numbers, 0;
 
 	for i, number in ipairs(numbers) do
-		assert(type(number) == 'number', "all paramters must be numbers");
+		assert(type(number) == 'number', "all parameters must be numbers");
 		hash_int = hash_int + (number % ((i - 1) + 100));
 	end
 
 	local alpha = self.alphabet;
-	local alpha_len = alpha:len();
+	local alpha_len = #alpha;
 
 	local lottery = gcap(alpha, hash_int % alpha_len);
 	local ret = lottery;
 	local last = nil;
 
-	for i, number in ipairs(numbers) do
-	-- for i=1, #numbers do
-	-- local number = numbers[i];
+	for i=1, #numbers do
+		local number = numbers[i];
 		alpha = consistent_shuffle(alpha, substr(strcat({lottery, self.salt, alpha}), 1, alpha_len));
 		last = hash(number, alpha);
 		ret = ret .. last;
 
 		if i < numbers_size then
-			number = number % (last:byte() + (i - 1));
-			ret = ret .. gcap(self.seps, (number % self.seps:len()));
+			number = number % (byte(last) + (i - 1));
+			ret = ret .. gcap(self.seps, (number % #self.seps));
 		end
 	end
 
-	local guards_len = self.guards:len();
-	if ret:len() < self.min_hash_length then
-		local guard_index = (hash_int + gcap(ret, 0):byte()) % guards_len;
+	local guards_len = #self.guards;
+	if #ret < self.min_hash_length then
+		local guard_index = (hash_int + byte(ret, 1)) % guards_len;
 		ret = gcap(self.guards, guard_index) .. ret;
 
-		if ret:len() < self.min_hash_length then
-			guard_index = (hash_int + gcap(ret, 2):byte()) % guards_len;
+		if #ret < self.min_hash_length then
+			guard_index = (hash_int + byte(ret, 3)) % guards_len;
 			ret = ret .. gcap(self.guards, guard_index);
 		end
 	end
 
 	local half_len, excess = floor(alpha_len * 0.5), 0; -- alpha_len / 2
-	while ret:len() < self.min_hash_length do
+	while #ret < self.min_hash_length do
 		alpha = consistent_shuffle(alpha, alpha);
-		ret = alpha:sub(half_len + 1) .. ret .. alpha:sub(1, half_len);
+		ret = strsub(alpha, half_len + 1) .. ret .. strsub(alpha, 1, half_len);
 
-		excess = (ret:len() - self.min_hash_length);
+		excess = (#ret - self.min_hash_length);
 		if excess > 0 then
 			excess = (excess * 0.5);
-			ret = ret:sub(excess + 1, (excess + self.min_hash_length));
+			ret = strsub(ret, excess + 1, (excess + self.min_hash_length));
 		end
 	end
 
@@ -140,7 +145,7 @@ function hash_mt:decode(hash)
 	-- TODO validate input
 	
 	local parts, index = {}, 1;
-	for part in hash:gmatch("[^".. self.guards .."]+") do
+	for part in gmatch(hash, "[^".. self.guards .."]+") do
 		parts[index] = part;
 		index = index + 1;
 	end
@@ -153,10 +158,10 @@ function hash_mt:decode(hash)
 	end
 
 	lottery = gcap(t, 0); -- put the first char in lottery
-	t = t:sub(2); -- then put the rest in t
+	t = substr(t, 2); -- then put the rest in t
 	
 	parts, index = {}, 1;
-	for part in t:gmatch("[^".. self.seps .."]+") do
+	for part in gmatch(t, "[^".. self.seps .."]+") do
 		parts[index] = part;
 		index = index + 1;
 	end
