1ebcf10457c0421a8be121ea6ecb2d18b4e78c52
Dwayne Slater <ds84182@gmail.com>
2015-11-28 13:32:46 -0500

    Use os.clock instead of os.time for benchmarking.

--- example.lua
+++ example.lua
@@ -4,7 +4,7 @@ local alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890
 local hashids = require("hashids");
 local h = hashids.new(nil, 8, alphabet);
 
-local ts = os.time();
+local ts = os.clock();
 print(h:encode(34, 2525));
 
 local hash;
@@ -13,4 +13,4 @@ for i = 1, 10000 do
 end
 
 print(h:encode(1337, 10, 666));
-print("elapsed time (in seconds):", os.time() - ts);
+print("elapsed time (in milliseconds):", (os.clock() - ts)*1000);
